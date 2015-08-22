00e2fd59dea60eb4621cfa8013fb69057e48cdb0
defacid <defacid@gmail.com>
2015-08-22 17:24:26 -0400

    Layout Finalization and Room Design
    Decided on how to build the rooms and display the page

--- js/app.js
+++ js/app.js
@@ -1,60 +1,118 @@
 (function(){
 	var app = angular.module("gameWorld", []);
 	
-	app.controller("RoomController", function(){
-		this.rooms = roomList;
-		this.type = "cool";
-	});
-	
-	app.controller("EventController", function(){
-		this.events = eventList;
-	});
-	
-	var direction = [
-		'north',
-		'east',
-		'south',
-		'west'
-	];
-	
-	var roomList = [
-		{
-			type: 'an open field',
-			description: 'You find yourself awaking to sharp pain covering your whole body. Alone in <strong>an open field</strong>, you struggle for breath and ache from the bruses on your upperbody and lacerations on your thighs and calves.',
+	app.controller("RoomController", ["$scope", function($scope){
+		$scope.room = {
+			event: 1,
+			type: 7,
+			poop: 0,
+			examined: 0,
 			directions: [
-				{canMove: 1, roomData: roomTypes[1]},
-				{canMove: 1, roomData: roomTypes[6]},
+				{canMove: 1, roomType: 1},
+				{canMove: 1, roomType: 6},
 				{canMove: 0, roomData: null},
 				{canMove: 0, roomData: null},
 			],
+			action: function(){
+				$scope.room.event = 0;
+			},
 		}
-	];
-	
-	var roomTypes = [
-		{
-			type: 'a dirt road',
-			description: 'TEST',
-		}, {
-			type: 'a dark forest',
-			description: 'TEST',
-		}, {
-			type: 'a large castle',
-			description: 'TEST',
-		}, {
-			type: 'an empty room',
-			description: 'TEST',
-		}, {
-			type: 'another empty room',
-			description: 'TEST',
-		}, {
-			type: 'a dungeon',
-			description: 'TEST',
-		}, {
-			type: 'a small farm',
-			description: 'TEST',
-		},
 		
-	];
-	
+		//Direction definitions (0: North, 1: East, 2: West, 3: South)
+		$scope.compass = [
+			'north',
+			'east',
+			'south',
+			'west'
+		];
+		
+		//Set of room templates
+		$scope.roomSet = [
+			{
+				type: 0,
+				name: 'NULL',
+				description: 'TEST',
+			}, {
+				type: 1,
+				name: 'a dark forest',
+				description: 'TEST',
+			}, {
+				type: 2,
+				name: 'a large castle',
+				description: 'TEST',
+			}, {
+				type: 3,
+				name: 'an empty room',
+				description: 'TEST',
+			}, {
+				type: 4,
+				name: 'another empty room',
+				description: 'TEST',
+			}, {
+				type: 5,
+				name: 'a dungeon',
+				description: 'TEST',
+			}, {
+				type: 6,
+				name: 'a small farm',
+				description: 'TEST',
+			}, {
+				type: 7,
+				name: 'an open field',
+				description: 'You are alone in an empty field.'
+			},
+			
+		];
+		
+		$scope.mapGrid = []
+		
+		//List of every defined room in the game
+		$scope.roomList = [
+			{
+				id: 0,
+				event: 0,
+				type: 0,
+				poop: 0,
+				examined: 0,
+				directions: [
+					{canMove: 0},
+					{canMove: 0},
+					{canMove: 0},
+					{canMove: 0},
+				],
+				action: function(){
+					alert("ERROR! ERROR! I NEED SCISSORS 61!");
+				},
+			}, {
+				id: 1,
+				event: 1,
+				type: 7,
+				poop: 0,
+				examined: 0,
+				directions: [
+					{canMove: 1, roomType: 1},
+					{canMove: 1, roomType: 6},
+					{canMove: 0, roomData: null},
+					{canMove: 0, roomData: null},
+				],
+				action: function(){
+					$scope.room.event = 0;
+				},
+			}, 
+		];
+		
+		//List of every text event that will happen in the game
+		$scope.eventList = [
+			{
+				id: 0,
+				description: '',
+			},{
+				id: 1,
+				description: 'You find yourself awaking to sharp pain covering your whole body. You struggle for breath and ache from the bruses on your upperbody and lacerations on your thighs and calves. Beaten and starved, your top priorities are tending to your wounds and empty stomach.',
+			},
+			
+		];
+	}]);
+
 	
 })();
